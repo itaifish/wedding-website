43c829be34c0041ee519b1c8a7557283d923e106
Itai Fish <chessandpotatoes@gmail.com>
2023-07-05 00:51:27 -0400

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1>Under Construction</h1>
+			<div className="card">
+				<p>This website will be finished soon, Itai is working as fast as he can</p>
+			</div>
+			<p className="read-the-docs">Thanks for being patient as we work towards celebrating our special day</p>
+		</>
+	);
+}
+
+export default App;
