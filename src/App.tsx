2d14703718ae7ece5b68d27b18de36b3df48066f
Itai Fish <chessandpotatoes@gmail.com>
2023-07-05 01:12:52 -0400

    comma->semicolon

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 		<>
 			<h1>Under Construction</h1>
 			<div className="card">
-				<p>This website will be finished soon, Itai is working as fast as he can</p>
+				<p>This website will be finished soon; Itai is working as fast as he can</p>
 			</div>
 			<p className="read-the-docs">Thanks for being patient as we work towards celebrating our special day</p>
 		</>
