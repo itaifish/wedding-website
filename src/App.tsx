30823b6f122b3a23ad546a86e04c2c05297dd86f
Itai Fish <chessandpotatoes@gmail.com>
2023-07-16 22:08:58 -0400

    Added rsvp form

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ function App() {
 			<h1>Under Construction</h1>
 			<div className="card">
 				<p>This website will be finished soon; Itai is working as fast as he can</p>
+				<p>
+					In the meantime, the{' '}
+					<a href="https://forms.gle/ya4wdpzhinCSvUo27" target="_blank">
+						RSVP form is available here.
+					</a>
+				</p>
 			</div>
 			<p className="read-the-docs">Thanks for being patient as we work towards celebrating our special day</p>
 		</>
