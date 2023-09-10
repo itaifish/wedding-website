7cc89e03fd5abeaa67d1456b7b6d0ce938874309
Itai Fish <chessandpotatoes@gmail.com>
2023-09-10 17:02:41 -0400

    Add location info

--- src/components/organisms/LocationAndInfo.tsx
+++ src/components/organisms/LocationAndInfo.tsx
@@ -4,8 +4,14 @@ import { links } from '../molecules/Header';
 const LocationAndInfo = () => {
 	return (
 		<div id={links.location.to} className="mx-auto w-1/2 py-20">
-			<h2 className="mx-auto text-4xl font-extrabold dark:text-white">Location and Info</h2>
-			<p className="text-base dark:text-white"></p>
+			<h2 className="mx-auto my-5 text-4xl font-extrabold dark:text-white">Location and Info</h2>
+			<p className="my-5 text-base dark:text-white">
+				The wedding will be held at{' '}
+				<a href="https://thegrahammill.com/" className="font-medium text-secondary hover:underline">
+					The Graham Mill
+				</a>{' '}
+				in Graham, North Carolina on December 21ˢᵗ 2021.
+			</p>
 			<GrahamMillMap />
 		</div>
 	);
