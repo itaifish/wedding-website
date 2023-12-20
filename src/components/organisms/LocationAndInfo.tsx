8f0965648f71c76f747ab5d0942e1de5c99af34f
Itai Fish <chessandpotatoes@gmail.com>
2023-12-20 13:57:04 -0500

    some small updates

--- src/components/organisms/LocationAndInfo.tsx
+++ src/components/organisms/LocationAndInfo.tsx
@@ -2,6 +2,7 @@ import GrahamMillMap from '../atoms/GrahamMillMap';
 import PrettyLink from '../atoms/PrettyLink';
 import { links } from '../molecules/Header';
 import Section from '../molecules/Section';
+import ParkingImage from '../../assets/location/parking.png';
 
 const LocationAndInfo = () => {
 	return (
@@ -13,7 +14,7 @@ const LocationAndInfo = () => {
 					<p className="my-5 text-base dark:text-white">
 						The wedding will be held at{' '}
 						<PrettyLink url="https://thegrahammill.com/" text="The Graham Mill" newTab={true} /> in Graham, North
-						Carolina on December 21ˢᵗ 2021. We have a block of rooms at the{' '}
+						Carolina on December 21ˢᵗ 2023. We have a block of rooms at the{' '}
 						<PrettyLink
 							url={'https://www.marriott.com/en-us/hotels/rdufm-fairfield-inn-and-suites-mebane/overview/'}
 							text="Fairfield Inn & Suites Mebane"
@@ -24,8 +25,13 @@ const LocationAndInfo = () => {
 							text="https://www.marriott.com/event-reservations/reservation-link.mi?id=1697832541095&key=GRP&app=resvlink"
 						/>
 					</p>
-
 					<GrahamMillMap />
+					<p className="my-5 text-base dark:text-white">
+						Parking in all of downtown is free and widely available. The areas circled in red are the closest available
+						spots to park, but everywhere with a parking symbol is allowed. Please do not park in the parking lot of the
+						Mexican restaurant next to the venue.
+					</p>
+					<img src={ParkingImage} />
 				</>
 			}
 		/>
